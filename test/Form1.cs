b7db89da8805fbad982a7000396ed3a945be450c
Jeff671 <114133466+Jeff671@users.noreply.github.com>
2022-11-28 23:29:24 +0800

    v1.7
    adding database

--- test/Form1.cs
+++ test/Form1.cs
@@ -7,18 +7,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection.Emit;
 
 namespace test
 {
     public partial class Form1 : Form
     {
+        int index = 1;
+
+        public class DBConfig
+        {
+            //log.db要放在【bin\Debug底下】      
+            public static string dbFile = Application.StartupPath + @"\log.db";
+
+            public static string dbPath = "Data source=" + dbFile;
+
+            public static SQLiteConnection sqlite_connect;
+            public static SQLiteCommand sqlite_cmd;
+            public static SQLiteDataReader sqlite_datareader;
+        }
+
+        private void Load_DB()
+        {
+            DBConfig.sqlite_connect = new SQLiteConnection(DBConfig.dbPath);
+            DBConfig.sqlite_connect.Open();// Open
+
+        }
+        private void Show_DB()
+        {
+            this.dataGridView1.Rows.Clear();
+
+            string sql = @"SELECT * from record;";
+            DBConfig.sqlite_cmd = new SQLiteCommand(sql, DBConfig.sqlite_connect);
+            DBConfig.sqlite_datareader = DBConfig.sqlite_cmd.ExecuteReader();
+
+            if (DBConfig.sqlite_datareader.HasRows)
+            {
+                while (DBConfig.sqlite_datareader.Read()) //read every data
+                {
+                    int _serial = Convert.ToInt32(DBConfig.sqlite_datareader["serial"]);
+                    int _date = Convert.ToInt32(DBConfig.sqlite_datareader["date"]);
+                    int _type = Convert.ToInt32(DBConfig.sqlite_datareader["type"]);
+                    string _name = Convert.ToString(DBConfig.sqlite_datareader["name"]);
+                    double _price = Convert.ToDouble(DBConfig.sqlite_datareader["price"]);
+                    double _number = Convert.ToDouble(DBConfig.sqlite_datareader["number"]);
+                    double _total = _price * _number;
+
+                    string _date_str = DateTimeOffset.FromUnixTimeSeconds(_date).ToString("yy-MM-dd hh:mm:ss");
+
+                    string _type_str = "";
+                    if (_type == 0)
+                    { _type_str = "進貨"; }
+                    else { _type_str = "出貨"; }
+
+                    index = _serial;
+                    DataGridViewRowCollection rows = dataGridView1.Rows;
+                    rows.Add(new Object[] { index, _date_str, _type_str, _name, _price, _number
+                                               , _total });
+                }
+                DBConfig.sqlite_datareader.Close();
+            }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
 
             Form2 f2 = new Form2();
             f2.ShowDialog();
+            Load_DB();
+            Show_DB();
+            this.numshow.Text = index.ToString();
+
         }
+        private void dataGridView1_CellDoubleClick(object sender
+                                                                      , DataGridViewCellEventArgs e)
+        {
+
+            DataGridViewCellCollection selRowData = dataGridView1.Rows[e.RowIndex].Cells;
+
+            string _type = "";
+            _type = Convert.ToString(selRowData[2].Value);
+
+            if (_type.Equals("進貨"))
+            {
+                inshipment.Checked = true;
+            }
+            else
+            {
+                outshipment.Checked = true;
+            }
+
+
+            this.shipmentbar.Text = Convert.ToString(selRowData[3].Value);
+            this.pricetext.Text = Convert.ToString(selRowData[4].Value);
+            this.mounttext.Text = Convert.ToString(selRowData[5].Value);
+            this.numshow.Text = Convert.ToString(selRowData[0].Value);
+
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -57,22 +148,46 @@ namespace test
 
         private void inputdata_Click(object sender, EventArgs e)
         {
-            string input_price = pricetext.Text;
-            string input_num = mounttext.Text;
-            string in_outlog = "";
-            string shipmentlog = shipmentbar.SelectedItem.ToString();
-            double price = Convert.ToDouble(input_price);
-            double num = Convert.ToDouble(input_num);
+            string _name = "";
+            long _date = 0;
+            int _stock_type = 0;
+            double _price = 0;
+            double _number = 0;
+            double _sum = 0;
+
+            // 抓取textbox的資料
+            _name = shipmentbar.Text;
+            _price = Convert.ToDouble(pricetext.Text);
+            _number = Convert.ToDouble(mounttext.Text);
+
+            _sum = _price * _number;
+            _date = DateTimeOffset.Now.ToUnixTimeSeconds();
             if (inshipment.Checked == true)
             {
-                in_outlog = "進貨";
+                _stock_type = 0;
             }
-            else in_outlog = "出貨";
-            showlog.Text = string.Format("總價:{0} {1} {2}", price * num, in_outlog, shipmentlog);
+            else
+            {
+                _stock_type = 1;
+            }
+            // update
+            this.index = this.index + 1;
 
-            DataGridViewRowCollection rows = dataGridView1.Rows;
-            DateTime date = DateTime.Now;
-            rows.Add(new object[] { "", date.ToString("yyyy/MM/dd HH:mm:ss"), in_outlog, shipmentlog,price,num,price * num});
+            // add item into database
+
+            string sql = @"INSERT INTO record (date, type, name,price,number)
+                VALUES( "
+                       + " '" + _date.ToString() + "' , "
+                       + " '" + _stock_type.ToString() + "' , "
+                       + " '" + _name.ToString() + "' , "
+                       + " '" + _price.ToString() + "' , "
+                       + " '" + _number.ToString() + "'   "
+                      + ");";
+            DBConfig.sqlite_cmd = new SQLiteCommand(sql, DBConfig.sqlite_connect);
+            DBConfig.sqlite_cmd.ExecuteNonQuery();
+
+            // show database in the gui
+            Show_DB();
         }
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
@@ -97,6 +212,47 @@ namespace test
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+
+        }
+
+        private void reNewdata_Click(object sender, EventArgs e)
+        {
+            string _name = "";
+            int _serial = 0;
+            int _stock_type = 0;
+            double _price = 0;
+            double _number = 0;
+
+            if (inshipment.Checked == true)
+            {
+                _stock_type = 0;
+            }
+            else
+            {
+                _stock_type = 1;
+            }
+
+            // 抓取textbox的資料
+            _name = shipmentbar.Text;
+
+
+            _price = Convert.ToDouble(pricetext.Text);
+            _number = Convert.ToDouble(mounttext.Text);
+            _serial = Convert.ToInt32(numshow.Text);
+
+
+            string sql = @"UPDATE record " +
+                      " SET name = '" + _name + "',"
+                        + " type = '" + _stock_type.ToString() + "' , "
+                        + " price = '" + _price.ToString() + "',"
+                        + " number = '" + _number.ToString() + "' "
+                        + "   where serial = " + _serial.ToString() + ";";
+
+
+            DBConfig.sqlite_cmd = new SQLiteCommand(sql, DBConfig.sqlite_connect);
+            DBConfig.sqlite_cmd.ExecuteNonQuery();
+            Show_DB();
+
 
         }
     }
